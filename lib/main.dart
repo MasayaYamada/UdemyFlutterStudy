f340ee7cfc0590c79b4a90ea3e2e16f2b0d831a1
MasayaYamada <masaya.yamada.stripes@gmail.com>
2020-07-15 18:19:57 +0900

    7/13 udemy class at 52

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fluttercompleteguide/answer.dart';
-
-import './question.dart';
+import 'package:fluttercompleteguide/quiz.dart';
+import 'package:fluttercompleteguide/result.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,35 +17,65 @@ class MyApp extends StatefulWidget {
 // State<MyApp> belong MyApp
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  final _questions = const [
+    {
+      'questionText': 'What\'s is your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 13},
+        {'text': 'Elephant', 'score': 11},
+        {'text': 'Rat', 'score': 5},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 10},
+        {'text': 'Mika', 'score': 14},
+        {'text': 'Yuka', 'score': 20},
+        {'text': 'Mike', 'score': 12},
+      ],
+    },
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s is your name?',
-      'What\'s your favorite animal',
-    ];
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions)
+              : Result()),
     );
   }
 }
